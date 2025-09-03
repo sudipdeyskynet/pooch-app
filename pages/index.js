af2d4780d4f8c7f977587016c6f59b98df7e5fda
Sudip <sudip@skynetonline.info>
2025-09-03 18:10:14 +0530

    index added

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>Pooch Profile App Running</h2>
+      <p>This is just a placeholder page for Vercel deployment.</p>
+    </div>
+  );
+}
